2c6c228392d817d89eefe6278d9bc3b2de9e3ea2
Muraker <contactpod@gmail.com>
2019-12-17 17:12:31 +0100

    Update Date 🌴

--- js/main.js
+++ js/main.js
@@ -24,3 +24,15 @@ $( '.icon, .avatar' ).each(function( index ) {
         top : Math.random() * ($('.main').height() - $(this).height())
     }).addClass('up');
 });
+
+
+$(window).resize(function(){
+
+    $( '.icon, .avatar' ).each(function( index ) {
+        $(this).css({
+            left : Math.random() * ($('.main').width() - $(this).width()),
+            top : Math.random() * ($('.main').height() - $(this).height())
+        }).addClass('up');
+    });
+
+});
